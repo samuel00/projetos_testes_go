da3fa0d83114466084d595aa8ab5ee36ad7565bc
samuel00 <ifpa.santana@gmail.com>
2018-11-12 19:13:52 -0200

    Conclusao do map e Criacao do function, metodo, interface

--- src/tour/map/map.go
+++ src/tour/map/map.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt" 
+)
 
 type Vertex struct {
 	Lat, Long float64
@@ -20,10 +22,48 @@ var m2 = map[string]Vertex{
 
 
 func main() {
+	fmt.Println("Tour 01");
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
 	fmt.Println(m["Bell Labs"])
 	fmt.Println(m2["Google"])
+
+	fmt.Println("");
+	fmt.Println("#######################################");
+	fmt.Println("Tour 02");
+	mapInt := make(map[string]int)
+
+	mapInt["Answer"] = 42
+	fmt.Println("The value:", mapInt["Answer"])
+
+	mapInt["Answer"] = 48
+	fmt.Println("The value:", mapInt["Answer"])
+
+	delete(mapInt, "Answer")
+	fmt.Println("The value:", mapInt["Answer"])
+
+	v, ok := mapInt["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+
+	fmt.Println("");
+	fmt.Println("#######################################");
+	fmt.Println("Tour 03");
+	m3 := make(map[string]int)
+
+	m3["Answer"] = 42;
+	fmt.Println("The value:", m3["Answer"]);
+
+	m3["Answer"] = 48;
+	fmt.Println("The value:", m3["Answer"]);
+
+	//delete(m3, "Answer")
+	//fmt.Println("The value:", m3["Answer"])
+
+	v2, k := m3["Answer"];
+		
+	if k == true{
+		fmt.Println("Valor encontrado:", v2);
+	}
 }
